cbb8b11f0ccfcdae808b1674061983ac7ca77f5b
jeonjnh <styxriver@gmail.com>
2023-05-13 22:41:22 +0200

    first commit : drop block

---
+++ tetromino.py
@@ -0,0 +1,34 @@
+from settings import *
+import random
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, tetromino, pos):
+        self.tetromino = tetromino
+        self.pos = vec(pos) + INIT_POS_OFFSET
+
+        super().__init__(tetromino.tetris.sprite_group)
+        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
+        self.image.fill('orange')
+
+        self.rect = self.image.get_rect()
+    
+    def set_rect_pos(self):
+        self.rect.topleft = self.pos * TILE_SIZE
+
+    def update(self):
+        self.set_rect_pos()
+
+class Tetromino:
+    def __init__(self, tetris):
+        self.tetris = tetris
+        self.shape = random.choice(list(TETROMINOS.keys()))
+        self.blocks = [Block(self, pos) for pos in TETROMINOS[self.shape]]
+
+    def move(self, direction):
+        move_direction = MOVING_DIRECTION[direction]
+        for block in self.blocks:
+            block.pos += move_direction
+
+    def update(self):
+        self.move(direction='down')
+        pg.time.wait(200)
