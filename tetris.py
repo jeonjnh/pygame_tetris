0fb05e5b19ee2be7e6379b6a183f17e30cccbf13
jeonjnh <styxriver@gmail.com>
2023-05-14 15:15:25 +0200

    second commit : finish tetris basic function

--- tetris.py
+++ tetris.py
@@ -6,7 +6,45 @@ class Tetris:
     def __init__(self, app):
         self.app = app
         self.sprite_group = pg.sprite.Group()
+        self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
+
+    def check_full_lines(self):
+        row = FIELD_H - 1
+        for y in range(FIELD_H - 1, -1, -1):
+            for x in range(FIELD_W):
+                self.field_array[row][x] = self.field_array[y][x]
+
+                if self.field_array[y][x]:
+                    self.field_array[row][x].pos = vec(x,y)
+
+            if sum(map(bool, self.field_array[y])) < FIELD_W:
+                row -= 1
+            else:
+                for x in range(FIELD_W):
+                    self.field_array[row][x].alive = False
+                    self.field_array[row][x] = 0
+
+    def put_tetromino_blocks_in_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+
+    def check_tetromino_landing(self):
+        if self.tetromino.landing:
+            self.put_tetromino_blocks_in_array()
+            self.tetromino = Tetromino(self)
+
+    def control(self, pressed_key):
+        if pressed_key == pg.K_LEFT:
+            self.tetromino.move(direction='left')
+        elif pressed_key == pg.K_RIGHT:
+            self.tetromino.move(direction='right')
+        elif pressed_key == pg.K_UP:
+            self.tetromino.rotate()
 
     def draw_grid(self):
         for x in range(FIELD_W):
@@ -15,7 +53,10 @@ class Tetris:
                              (x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
     
     def update(self):
-        self.tetromino.update()
+        if self.app.anim_trigger:
+            self.check_full_lines()
+            self.tetromino.update()
+            self.check_tetromino_landing()
         self.sprite_group.update()
 
     def draw(self):
